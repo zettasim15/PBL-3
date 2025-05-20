f0728af2e848311146915f1bb8b12c3080e9af3f
gisella1109 <gisellalaudila42@gmail.com>
2025-05-20 15:04:18 +0700

    Redesign dashboard layout, add new presence and calendar views, cleanup old files

--- routes/web.php
+++ routes/web.php
@@ -8,3 +8,28 @@ use App\Http\Controllers\ListBarangController;
 Route::get('/', [HomeController::class, 'index']);
 Route::get('/contact', [HomeController::class, 'contact']);
 Route::get('/list-barang', [ListBarangController::class, 'tampilkan']);
+Route::get('/contact', [HomeController::class, 'contact']);
+
+Route::get('/login', function () {
+    return view('login');
+});
+
+Route::get('/dashboard_member', function () {
+    return view('dashboard');
+});
+
+Route::get('/schedule', function () {
+    return view('schedule');
+});
+
+Route::get('/kalendermember', function () {
+    return view('Kalendermember');
+});
+
+Route::get('/teams', function () {
+    return view('teams');
+});
+
+Route::get('/presence', function () {
+    return view('presence');
+});
